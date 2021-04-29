a9449b22eede92c0d918bdefb6db7abdd6796b33
Ajaya1000 <ajaymati1000@gmail.com>
2021-04-29 16:34:32 +0530

    fixed problem withh muitheme

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from '@material-ui/styles';
 import React from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import { createMuiTheme } from '@material-ui/core';
 
 import Routes from './Routes';
 import configureStore from './modules/store';
@@ -9,13 +10,13 @@ const initialState = window.REDUX_INITIAL_DATA;
 console.log(initialState);
 const reduxStore = configureStore(initialState);
 
-const theme = {
+const theme = createMuiTheme({
     primary: {
         text: '',
         textGray: '',
         background: '',
     },
-};
+});
 
 function App() {
     return (
